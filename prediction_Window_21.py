94eb4d7efc78db186048637ac514cf6a5854c456
wawancenggoro <wawancenggoro@gmail.com>
2018-11-17 20:32:54 +0700

    update prediction_Window_21.py

--- prediction_Window_21.py
+++ prediction_Window_21.py
@@ -220,7 +220,7 @@ print('Validation label shape: ', valid_Y.shape)
 # Neural Network Modeling
 
 epochs = 200
-l2_rate = 0.1
+l2_rate = 0.01
 
 inp = Input(shape=(21,))
 x = Embedding(20, 20, input_length=21)(inp)
@@ -242,9 +242,9 @@ x = BatchNormalization()(x)
 predictions = Dense(2, activation='softmax')(x)
 model = Model(inputs=inp, outputs=predictions)
 
-lr = 0.0001
+lr = 0.001
 # opt = keras.optimizers.SGD(lr=lr, momentum=0.9, decay=1e-4, nesterov=True)
-opt = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=1e-6, amsgrad=False)
+opt = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=1e-4, amsgrad=False)
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 # def scheduler(epoch):
